8ac03c86777cb9eed0ba82386f2a6c63f89b1446
schess <schess@us.ibm.com>
2019-02-21 12:28:46 -0500

    dev

--- bot.js
+++ bot.js
@@ -74,11 +74,11 @@ bot.on('message', function(user, userID, channelID, message, event) {
                     if (args[1]) {
                         votes[userID].topics[args[0]] = {};
                         votes[userID].topics[args[0]].vote = args[1];
-                        votes[userID].topics[args[0]].timestamp = Date.now().toLocaleTimeString();
+                        votes[userID].topics[args[0]].timestamp = new Date().toLocaleTimeString();
                     } else {
                         votes[userID].topics.current = {};
                         votes[userID].topics.current.vote = args[0];
-                        votes[userID].topics.current.timestamp = Date.now().toLocaleTimeString();
+                        votes[userID].topics.current.timestamp = new Date().toLocaleTimeString();
                     }
                     bot.sendMessage({
                         to: channelID,
